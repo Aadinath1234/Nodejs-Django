807f4edf3c14846909dbcd75bd1cef66711f910f
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-06-17 12:36:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,45 @@ import Login from "./components/Login/Login";
 
 import Home from "./components/Home/Home";
 import Task from "./components/Task/Task";
+import ProtectedRoute from "./components/Protectedroute";
+import ProtectedRedirection from "./components/ProtectedRedirection";
 
 function App() {
   return (
     <div className=" overflow-x-hidden">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           
-          <Route path="/task" element={<Task />} />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRedirection>
+                <Home />
+              </ProtectedRedirection>
+            } /> 
+
+
+
+         
+
+          <Route 
+           path="/login" 
+           element={
+            <ProtectedRedirection>
+              <Login />
+            </ProtectedRedirection>
+           } 
+        />
+           
+          
+          <Route
+            path="/task" 
+            element={
+              <ProtectedRoute>
+                <Task />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </div>
